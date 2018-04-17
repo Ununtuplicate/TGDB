de43a567ba1f4c8e610ec9a93d55f9cd266421ea
Dyplomatism <chrisalexbauer@gmail.com>
2018-04-17 20:09:38 +0200

    Http404 get_object_or_404 get_list_or_404; added these 404 methods to views.py; reverse post view order via (-id); changes to app url pattern

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     re_path('category/(?P<category_slug>[\w-]+)/', views.post_by_category, name='post_by_category'),
     re_path('tag/(?P<tag_slug>[\w-]+)/', views.post_by_tag, name='post_by_tag'),
-    re_path('(?P<pk>\d+)/', views.post_detail, name='post_detail'),
+    re_path('(?P<pk>\d+)/(?P<post_slug>[\w\d-]+)', views.post_detail, name='post_detail'),
     path('', views.post_list, name='post_list'),
 ]
