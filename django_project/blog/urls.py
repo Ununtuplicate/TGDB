e723fe44094c2b7371695d2a1f14072d37746649
Dyplomatism <chrisalexbauer@gmail.com>
2018-04-17 19:08:20 +0200

    admin, views, post_list, post_detail, post_by_category, post_by_tag

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, re_path
-from blog import views
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='blog_index'),
+    re_path('category/(?P<category_slug>[\w-]+)/', views.post_by_category, name='post_by_category'),
+    re_path('tag/(?P<tag_slug>[\w-]+)/', views.post_by_tag, name='post_by_tag'),
+    re_path('(?P<pk>\d+)/', views.post_detail, name='post_detail'),
+    path('', views.post_list, name='post_list'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Author, Tag, Category, Post
 
-# Create your views here.
+def index(request):
+    return HttpResonse("Hello Django")
+
+# view function to display a list of posts
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+# view function to display a single post
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+#view function to display post by category
+def post_by_category(request, category_slug):
+    category = Category.objects.get(slug=category_slug)
+    posts = Post.objects.filter(category__slug=category_slug)
+    context = {
+        'category': category,
+        'posts': posts
+    }
+    print(category)
+    return render(request, 'blog/post_by_category.html', context)
+
+# view function to display post by tag
+def post_by_tag(request, tag_slug):
+    tag = Tag.objects.get(slug=tag_slug)
+    posts = Post.objects.filter(tags__name=tag)
+    context = {
+        'tag': tag,
+        'posts': posts
+    }
+    return render(request, 'blog/post_by_tag.html', context)
