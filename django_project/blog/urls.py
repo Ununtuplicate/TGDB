e75d30f7ed33cfe74f27a28295f8b672e77b2065
Dyplomatism <chrisalexbauer@gmail.com>
2018-04-26 13:45:38 +0200

    added test session cookie; save, access, delete session data methods with example info

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,6 +2,11 @@ from django.urls import path, re_path, include
 from . import views
 
 urlpatterns = [
+    path('save-session-data/', views.save_session_data, name="save_session_data"),
+    path('access-session-data/', views.access_session_data, name="access_session_data"),
+    path('delete-session-data/', views.delete_session_data, name="delete_session_data"),
+    path('test-delete/', views.test_delete, name='test_delete'),
+    path('test-session/', views.test_session, name='test_session'),
     path('feedback/', views.feedback, name='feedback'),
     re_path('category/(?P<category_slug>[\w-]+)/', views.post_by_category, name='post_by_category'),
     re_path('tag/(?P<tag_slug>[\w-]+)/', views.post_by_tag, name='post_by_tag'),
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -65,3 +65,48 @@ def post_list(request):
     posts = Post.objects.order_by("-id").all()
     posts = helpers.pg_records(request, posts, 5)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+# test cookie function to see whether user accepts cookies 
+def test_session(request):
+    request.session.set_test_cookie()
+    return HttpResponse("Testing session cookie")
+
+def test_delete(request):
+    if request.session.test_cookie_worked():
+        request.session.delete_test_cookie()
+        response = HttpResponse("Cookie test passed")
+    else:
+        response = HttpResponse("Cookie test failed")
+    return response
+
+# reading and writing session data
+def save_session_data(request):
+    # set new data
+    request.session['id'] = 1
+    request.session['name'] = 'root'
+    request.session['password'] = 'rootpass'
+    return HttpResponse("Session Data Saved")
+
+def access_session_data(request):
+    response = ""
+    if request.session.get('id'):
+        response += "Id : {0} <br>".format(request.session.get('id'))
+    if request.session.get('name'):
+        response += "Name : {0} <br>".format(request.session.get('name'))
+    if request.session.get('password'):
+        response += "Password : {0} <br>".format(request.session.get('password'))
+        
+    if not response:
+        return HttpResponse("No session data")
+    else:
+        return HttpResponse(response)
+    
+def delete_session_data(request):
+    try:
+        del request.session['id']
+        del request.session['name']
+        del request.session['password']
+    except KeyError:
+        pass
+    
+    return HttpResponse("Session Data Cleared")
